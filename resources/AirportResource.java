c07380ce64afabb43473e26900edf88e10945997
szymaneczek <szymaneczek@gmail.com>
2023-06-15 11:04:01 +0200

    created class with Polish Airports and flight connections between towns

--- resources/AirportResource.java
+++ resources/AirportResource.java
@@ -1,8 +1,10 @@
 package resources;
 
 import domain.Airport;
+import domain.Flight;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class AirportResource {
 
@@ -26,5 +28,35 @@ public class AirportResource {
         airports.put("RZE", rze);
         airports.put("KRK", krk);
         airports.put("KTW", ktw);
-    }
+
+        addFlight(new Flight(gdn, waw));
+        addFlight(new Flight(gdn, rze));
+        addFlight(new Flight(gdn, krk));
+        addFlight(new Flight(gdn, ktw));
+        addFlight(new Flight(epsd, waw));
+        addFlight(new Flight(waw, luz));
+        addFlight(new Flight(waw, gdn));
+        addFlight(new Flight(waw, rze));
+        addFlight(new Flight(waw, epsd));
+        addFlight(new Flight(waw, krk));
+        addFlight(new Flight(waw, ktw));
+        addFlight(new Flight(luz, waw));
+        addFlight(new Flight(rze, gdn));
+        addFlight(new Flight(rze, waw));
+        addFlight(new Flight(krk, gdn));
+        addFlight(new Flight(krk, waw));
+        addFlight(new Flight(ktw, gdn));
+        addFlight(new Flight(ktw, waw));
+        }
+
+        public void addFlight(Flight flight){
+        Airport from = flight.getFrom();
+        Airport to = flight.getTo();
+
+        airports.get(from.getId()).addOutgoingFlightLocation(to);
+        airports.get(to.getId()).addIncomingFlightLocation(from);
+
+        }
+
+        public Map<String,Airport> getALL(){return airports;}
 }
