c458c5233a55e408101d1f49bf2ae320c16302a8
szymaneczek <szymaneczek@gmail.com>
2023-06-15 10:35:09 +0200

    created public class Flight with Constructors

--- domain/Flight.java
+++ domain/Flight.java
@@ -1,4 +1,44 @@
 package domain;
 
+import java.util.Objects;
+
 public class Flight {
+
+    private Airport from;
+    private Airport to;
+
+    public Flight(Airport from, Airport to){
+        this.from = from;
+        this.to = to;
+    }
+
+    public Airport getFrom(){
+        return from;
+    }
+
+    public void setFrom(Airport from) {this.from = from;}
+
+    public Airport getTo(){return to;}
+
+    public void setTo(Airport to) {this.to = to;}
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "from=" + from +
+                ", to=" + to +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Flight flight)) return false;
+        return Objects.equals(from, flight.from) && Objects.equals(to, flight.to);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(from, to);
+    }
 }
