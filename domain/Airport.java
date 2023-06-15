ff7fb9ef8e9d5143edf1b17acea366bb37aaad7e
szymaneczek <szymaneczek@gmail.com>
2023-06-15 10:22:06 +0200

    added Constructors (Setters, Getters)

--- domain/Airport.java
+++ domain/Airport.java
@@ -9,4 +9,100 @@ public class Airport {
     private String location;
     private Set <Airport> incomingFlightsLocations = new HashSet<>();
     private Set <Airport> outgoingFlightsLocations = new HashSet<>();
+
+    public Airport(String id, String location){
+        this.id = id;
+        this.location = location;
+    }
+
+    public String getId() {return id;}
+
+    public void setId(String id) { this.id = id;}
+
+    public String getLocation() {return location;}
+
+    public void setLocation(String location) {this.location = location;}
+
+    public Set<Airport> getIncomingFlightsLocations() {return incomingFlightsLocations;}
+
+    public void setIncomingFlightsLocations(Set<Airport> incomingFlightsLocations){
+        this.incomingFlightsLocations = incomingFlightsLocations;
+    }
+    public Set<Airport> getOutgoingFlightsLocations(){ return outgoingFlightsLocations;}
+
+    public void addIncomingFlightLocation(Airport airport){
+        incomingFlightsLocations.add(airport);
+    }
+
+    public void addOutgoingFlightLocation(Airport airport){
+        outgoingFlightsLocations.add(airport);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
++
+
+
+
+
+
+
+
+
