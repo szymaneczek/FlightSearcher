ccc7cbadf64e80a1e71ca7aff34ecabe7b0a8997
szymaneczek <szymaneczek@gmail.com>
2023-06-16 10:17:09 +0200

    FlightSearchService

--- services/FlightSearchService.java
+++ services/FlightSearchService.java
@@ -1,7 +1,11 @@
 package services;
 
 import domain.Airport;
+import domain.Flight;
+import dto.FlightSearchDto;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class FlightSearchService {
@@ -14,5 +18,25 @@ public class FlightSearchService {
         airports = airportService.getAllAirports();
     }
 
+    public FlightSearchDto findFlights(String from, String to){
+        Airport airportFrom = airports.get(from.toUpperCase());
+        Airport airportTo = airports.get(to.toUpperCase());
+
+        Flight directFlight = null;
+        List<Flight> jointFlights = new ArrayList<>();
+
+        if (airportFrom.getOutgoingFlightsLocations().contains(airportTo)){
+            directFlight = new Flight(airportFrom, airportTo);
+        }
+
+        airportFrom.getOutgoingFlightsLocations().stream()
+                .filter(airport -> airport.getOutgoingFlightsLocations().contains(airportTo))
+                .forEach(airport -> {
+                    jointFlights.add(new Flight(airportFrom, airport));
+                    jointFlights.add(new Flight(airport, airportTo));
+                });
+
+        return new FlightSearchDto(directFlight, jointFlights);
+    }
 
 }
